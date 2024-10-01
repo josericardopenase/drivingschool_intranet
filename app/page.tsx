8bbc89182ad5d6ed010229085198c840e5d85d7e
Pepe <pepe@MBP-de-Pepe.192.168.0.1>
2024-10-01 13:43:31 +0100

    finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
-import { BiChat, BiLock, BiLockAlt, BiSolidLock } from "react-icons/bi";
+import {
+  BiChat,
+  BiFolder,
+  BiLock,
+  BiLockAlt,
+  BiSolidFolder,
+  BiSolidLock,
+} from "react-icons/bi";
 import { FaCloud, FaMessage } from "react-icons/fa6";
 import { IoIosLock } from "react-icons/io";
 import { IoChatbubble } from "react-icons/io5";
@@ -12,7 +19,11 @@ export default function Home() {
           <h1 className="text-center text-3xl font-bold">AUTOESCUELA ECO</h1>
           <h2 className="mt-3 text-xl font-bold text-green-500">PEPITO 1.0</h2>
         </section>
-        <section className="mt-14 grid grid-cols-3 w-full gap-5">
+
+        <h1 className="text-xl mt-10 font-semibold text-gray-500">
+          Aplicaciones
+        </h1>
+        <section className="mt-3 grid grid-cols-3 w-full gap-5">
           <a
             href="https://documentos.autoescuelaseco.cloud/"
             target="_blank"
@@ -24,8 +35,29 @@ export default function Home() {
             <p className="font-semibold text-xl text-gray-700 mt-5">
               Gestor de documentos
             </p>
-            <p className="text-sm text-gray-700 mt-0 text-blue-400">
-              Nextcloud
+            <p className="text-sm font-semibold mt-0 text-blue-400">
+              Documentos
+            </p>
+          </a>
+        </section>
+
+        <h1 className="text-xl mt-10 font-semibold text-gray-700">
+          Accesos directos
+        </h1>
+        <section className="mt-3 grid grid-cols-3 w-full gap-5">
+          <a
+            href="https://documentos.autoescuelaseco.cloud/index.php/f/2856"
+            target="_blank"
+            className="bg-gray-100 p-10 rounded-3xl w-full "
+          >
+            <div className="h-[50px] w-[50px] bg-yellow-400 flex justify-center items-center rounded-lg text-white">
+              <BiSolidFolder size="30" />
+            </div>
+            <p className="font-semibold text-xl text-gray-700 mt-5">
+              PROTOCOLOS
+            </p>
+            <p className="text-sm mt-0 text-yellow-400 font-semibold">
+              Documentos
             </p>
           </a>
         </section>
